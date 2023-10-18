8716cfd8c076ae30ea48724911dc45ccee66ecbd
jishnu Rahul <jishnurahulkanisetty@gmail.com>
2023-10-18 17:35:40 -0400

    SelectionSort

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -7,10 +7,12 @@ def SelectionSort(inputList,ascending= True):
     for i in range(len(inputList)):
         mid_index = i
         for j in range(i+1,len(inputList)):
-            if inputList[mid_index] > inputList[j]:
+            if ascending and inputList[mid_index] > inputList[j]:
+                mid_index = j
+            elif (not ascending) and inputList[mid_index] < inputList[j]:
                 mid_index = j
         if mid_index != i:
             inputList[i],inputList[mid_index] = inputList[mid_index],inputList[i]
     return inputList
 
-print(SelectionSort([2,1,5,4,8,9,6,3]))
+print(SelectionSort([2,1,5,4,8,9,6,3],ascending= False))
