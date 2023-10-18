0da4cbccd5f471ef890e7e09616b6fd4552002ae
jishnu Rahul <jishnurahulkanisetty@gmail.com>
2023-10-18 12:35:03 -0400

    Bubble Sort

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,3 +1,13 @@
+#BUBBLE SORT
+#Compares two numbers in the list and then swaps the elements 
+#Time Complexity : O(N LogN)
+#Space Complexity : O(1)
+
 def BubbleSort(inputList):
-    
+    for i in range(len(inputList)):
+        for j in range(i, len(inputList)):
+            if inputList[i] > inputList[j]:
+                inputList[i],inputList[j] = inputList[j],inputList[i]
     return inputList
+
+print(BubbleSort([2,1,8,9,4,6]))
