762e29dbd2d6e1b5dff6f525ba09a5b38f8edc48
jishnu Rahul <jishnurahulkanisetty@gmail.com>
2023-10-18 17:14:30 -0400

    Selection Sort

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -1,5 +1,16 @@
+#BUBBLE SORT
+#swap the minimum value into the position then move to the next index
+#Time Complexity : O(N^2)
+#Space Complexity : O(1)
+
 def SelectionSort(inputList,ascending= True):
-    
+    for i in range(len(inputList)):
+        mid_index = i
+        for j in range(i+1,len(inputList)):
+            if inputList[mid_index] > inputList[j]:
+                mid_index = j
+        if mid_index != i:
+            inputList[i],inputList[mid_index] = inputList[mid_index],inputList[i]
     return inputList
 
 print(SelectionSort([2,1,5,4,8,9,6,3]))
