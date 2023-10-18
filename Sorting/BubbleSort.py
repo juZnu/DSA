4a0ae4c8b1e1d22e9f53550a9177c3130f9a7049
jishnu Rahul <jishnurahulkanisetty@gmail.com>
2023-10-18 12:38:56 -0400

    Solved

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -3,11 +3,15 @@
 #Time Complexity : O(N LogN)
 #Space Complexity : O(1)
 
-def BubbleSort(inputList):
+def BubbleSort(inputList,ascending = True):
     for i in range(len(inputList)):
         for j in range(i, len(inputList)):
-            if inputList[i] > inputList[j]:
-                inputList[i],inputList[j] = inputList[j],inputList[i]
+            if ascending:
+                if inputList[i] > inputList[j]:
+                    inputList[i],inputList[j] = inputList[j],inputList[i]
+            else:
+                if inputList[i] < inputList[j]:
+                    inputList[i],inputList[j] = inputList[j],inputList[i]
     return inputList
 
 print(BubbleSort([2,1,8,9,4,6]))
